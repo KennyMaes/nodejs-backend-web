acf86847f59675a7587f219b6635bfd9c1ecd021
KennyMaes <kennymaes@live.be>
2024-08-03 11:06:19 +0200

    :sparkles: add better error handling to pass the status for custom errors + add notFoundException error

--- src/util/errorHandler.ts
+++ src/util/errorHandler.ts
@@ -1,9 +1,9 @@
 import {ErrorRequestHandler} from 'express';
-import {BadRequestError} from './exceptions';
+import {BadRequestError, CustomError} from './exceptions';
 
 const errorHandler: ErrorRequestHandler = (err: Error, req, res, next) => {
     console.error(err.stack); // Log the error for debugging
-    const status = err instanceof BadRequestError ? 400 : 500;
+    const status = err instanceof CustomError ? err.status : 500;
     const message = err.message || 'Internal Server Error';
 
     res.status(status).json({ error: message });
--- src/util/exceptions.ts
+++ src/util/exceptions.ts
@@ -1,6 +1,22 @@
-export class BadRequestError extends Error {
-    constructor(message: string) {
+export class CustomError extends Error {
+    public status: number
+
+    constructor(message: string, status: number) {
         super(message);
+        this.status = status;
+    }
+}
+
+export class BadRequestError extends CustomError {
+    constructor(message: string) {
+        super(message, 400);
         this.name = 'BadRequestError';
+    }
+}
+
+export class NotFoundError extends CustomError {
+    constructor(message: string) {
+        super(message, 404);
+        this.name = 'NotFoundError';
     }
 }
