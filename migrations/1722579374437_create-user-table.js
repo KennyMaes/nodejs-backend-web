3cc78dcf3e486b82a14818a1436e023ab75d3499
KennyMaes <kennymaes@live.be>
2024-08-03 10:06:43 +0200

    :sparkles: Implement down migration to drop the users table

--- migrations/1722579374437_create-user-table.js
+++ migrations/1722579374437_create-user-table.js
@@ -26,4 +26,6 @@ exports.up = (pgm) => {
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+    pgm.dropTable('users')
+};
